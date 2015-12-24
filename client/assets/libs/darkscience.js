ba6b8c68225879b74a3c63891e3eb4c14436a6ba
Liothen <liothen@flagrun.net>
2015-12-24 13:09:09 -0800

    quick jQuery replace based off which channel selected

---
+++ client/assets/libs/darkscience.js
@@ -0,0 +1,6 @@
+$(document).on('click', '#c_flagrun', function() { $("#server_select_channel").val("#flagrun") });
+$(document).on('click', '#c_darkscience', function() { $("#server_select_channel").val("#darkscience") });
+$(document).on('click', '#c_darchoods', function() { $("#server_select_channel").val("#darchoods") });
+$(document).on('click', '#c_libsecure', function() { $("#server_select_channel").val("#libsecure") });
+$(document).on('click', '#c_treehouse', function() { $("#server_select_channel").val("#treehouse") });
+$(document).on('click', '#c_chasenet', function() { $("#server_select_channel").val("#chasenet") });
